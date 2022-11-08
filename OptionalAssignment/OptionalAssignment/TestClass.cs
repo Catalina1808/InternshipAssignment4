6a893915fad1dab63e9b597178fbb4cc8a26407d
Cătălina Grămadă <catalina.g1808@gmail.com>
2022-11-08 17:38:56 +0200

    Practiced strings, date and times

--- OptionalAssignment/OptionalAssignment/TestClass.cs
+++ OptionalAssignment/OptionalAssignment/TestClass.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +13,43 @@ namespace Assignment
         public static void Main()
         {
             //testing the App Tag Interpreter
+
             OptionalAssignment optionalAssignment = new OptionalAssignment();
             Console.WriteLine(optionalAssignment.AppTagInterpreter("<app></app></app><app><app>"));
 
-           
+
+            //practicing dates & strings
+
+            string text = "Hello World!";
+            string[] array = text.Split(" ");
+
+            foreach (var word in array)
+            {
+                Console.WriteLine(word);
+            }
+
+            string joined = string.Join("@", array);
+            Console.WriteLine(joined);
+
+            TimeSpan time = new TimeSpan(1, 60, 60);
+            Console.WriteLine(time);
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+
+            DateTimeOffset dateOffsetNow = DateTimeOffset.Now;
+            DateTimeOffset dateOffsetUtcNow = DateTimeOffset.UtcNow;
+
+            TimeZone localZone = TimeZone.CurrentTimeZone;
+
+            Console.WriteLine("Standard time name:" + localZone.StandardName);
+            Console.WriteLine("Daylight saving time name:" + localZone.DaylightName);
+
+            CultureInfo cultureInfo = new CultureInfo("ro-RO", false);
+            Console.WriteLine($"Culture: {cultureInfo.DisplayName}");
+            CultureInfo cultureInfo2 = new CultureInfo("en-US", false);
+            Console.WriteLine("Culture " + cultureInfo2.NativeName);
+
 
         }
     }
