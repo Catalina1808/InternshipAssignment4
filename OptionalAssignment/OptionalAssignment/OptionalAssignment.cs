722be0ce52fa29d2d677b5cc2e01dea7afe65d64
Cătălina Grămadă <catalina.g1808@gmail.com>
2022-11-08 17:06:54 +0200

    Added the optional assignment

---
+++ OptionalAssignment/OptionalAssignment/OptionalAssignment.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class OptionalAssignment
+    {
+        public int AppTagInterpreter(string text){
+
+            int wrongOpenedTags = 0;
+            int wrongClosedTags = 0;
+
+            while (text.Length > 0)
+            {
+                if (text.StartsWith("<app>"))
+                {
+                    wrongOpenedTags++;
+                    text = text.Substring(5);
+                } 
+                else if (text.StartsWith("</app>"))
+                {
+                    if(wrongOpenedTags == 0)
+                    {
+                        wrongClosedTags++;
+                    }
+                    else
+                    {
+                        wrongOpenedTags--;
+                    }
+                    text = text.Substring(6);
+                }
+            }
+
+            return wrongOpenedTags + wrongClosedTags;
+
+        }
+    }
+}
---
+++ OptionalAssignment/OptionalAssignment/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class TestClass
+    {
+        public static void Main()
+        {
+            //testing the App Tag Interpreter
+            OptionalAssignment optionalAssignment = new OptionalAssignment();
+            Console.WriteLine(optionalAssignment.AppTagInterpreter("<app></app></app><app><app>"));
+
+           
+
+        }
+    }
+}
